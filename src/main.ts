37e9b9b95c22642547644a1041b9d917c965d381
cleong98 <zhuzhuloh1998@gmail.com>
2023-07-21 16:23:00 +0800

    initial vite setting and ssg setting

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,55 @@
-import { createApp } from 'vue'
+import '@unocss/reset/tailwind.css'
+import 'uno.css'
+import Nprogress from 'nprogress'
+import FloatingVue from 'floating-vue'
+import autoRoutes from 'pages-generated'
+import { setupRouterScroller } from 'vue-router-better-scroller'
+import { ViteSSG } from 'vite-ssg'
+import dayjs from 'dayjs'
+import LocalizedFormat from 'dayjs/plugin/localizedFormat'
+
+// import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+const routes = autoRoutes.map((i) => {
+  return {
+    ...i,
+    alias: i.path.endsWith('/')
+      ? `${i.path}index.html`
+      : `${i.path}.html`
+  }
+})
+
+// createApp(App).mount('#app')
+
+export const createApp = ViteSSG(App, {
+  routes,
+}, ({ router, app, isClient }) => {
+  dayjs.extend(LocalizedFormat)
+  app.use(FloatingVue)
+  if (isClient) {
+    const html = document.querySelector('html')!
+    setupRouterScroller(router, {
+      selectors: {
+        body(ctx) {
+          // only do the sliding transition when the scroll position is not 0
+          if (ctx.savedPosition?.top)
+            html.classList.add('no-sliding')
+          else
+            html.classList.remove('no-sliding')
+          return true
+        }
+      },
+      behavior: 'auto',
+    })
+
+    router.beforeEach(() => {
+      Nprogress.start()
+    })
+
+    router.afterEach(() => {
+      Nprogress.done()
+    })
+  }
+}) 
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,97 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import Unocss from 'unocss/vite'
+import Pages from 'vite-plugin-pages';
+import Markdown from 'vite-plugin-vue-markdown'
+import AutoImport from 'unplugin-auto-import/vite'
+import Component from 'unplugin-vue-components/vite'
+import IconResolver from 'unplugin-icons/resolver'
+import Icons from 'unplugin-icons/vite'
+import Inspect from 'vite-plugin-inspect'
+import SVG from 'vite-svg-loader'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  resolve: {
+    alias: [
+      {
+        find: '~/',
+        replacement: `${new URL('src', import.meta.url)}`
+      }
+    ]
+  },
+
+  optimizeDeps: {
+    include: [
+      'vue',
+      'vue-router',
+      '@vueuse/core',
+      'dayjs',
+      'dayjs/plugin/localizedFormat'
+    ]
+  },
+
+  plugins: [
+    Unocss(),
+
+    vue({
+      include: [/\.vue$/, /\.md$/],
+      script: {
+        defineModel: true,
+      }
+    }),
+
+    Pages({
+      extensions: ['vue', 'md'],
+      dirs: 'pages',
+      // extendRoute(route) {
+      //   const path = new URL(route.components.slice(1), import.meta.url).pathname;
+      //   return route;
+      // }
+    }),
+
+    Markdown(),
+
+    AutoImport({
+      include: [
+        'vue',
+        'vue-router',
+        '@vueuse/core',
+        '@unhead/vue',
+      ]
+    }),
+
+    Component({
+      extensions: ['vue', 'md'],
+      dts: true,
+      include: [/\.vue$/, /\.vue\/?vue/, /\.md$/],
+      resolvers: [
+        IconResolver({
+          prefix: '',
+        })
+      ]
+    }),
+
+    Inspect(),
+
+    Icons({
+      defaultClass: 'inline',
+      defaultStyle: 'vertical-align:sub;'
+    }),
+
+    SVG({
+      svgo: false,
+      defaultImport: 'url',
+    }),
+  ],
+
+  build: {
+    rollupOptions: {
+      onwarn(warning, next) {
+        if (warning.code === 'UNUSED_EXTERNAL_IMPORT') {
+          next(warning);
+        }
+      }
+    }
+  },
 })
